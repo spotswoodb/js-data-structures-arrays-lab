113a496dd50392da1a7288cadc13338e4758a8ef
Ben Spotswood <spotswoodb@gmail.com>
2021-08-18 22:39:15 -0500

    Done.

--- index.js
+++ index.js
@@ -1,34 +1,35 @@
 // Write your solution here!
- var drivers = ["Milo", "Otis", "Garfield"];
 
- function destructivelyAppendDriver(name) {
-     drivers.push(name);
- }
+const drivers = ["Milo", "Otis", "Garfield"];
 
- function destructivelyPrependDriver(name) {
-     drivers.unshift(name);
- }
-
- function destructivelyRemoveLastDriver() {
-    drivers.pop();
- }
-
- function destructivelyRemoveFirstDriver() {
-     drivers.shift();
- }
-
- function appendDriver(name) {
-     return [...drivers, name];
- }
-
- function prependDriver(name) {
-     return [name, ...drivers];
- }
-
- function removeLastDriver() {
-     return (drivers.slice(0, drivers.length-1));
+function destructivelyAppendDriver(name){
+    drivers.push(name);
 }
 
-function removeFirstDriver() {
-    return (drivers.slice(1));
+function destructivelyPrependDriver(name){
+    drivers.unshift(name)
+}
+
+function destructivelyRemoveLastDriver(){
+    drivers.pop()
+}
+
+function destructivelyRemoveFirstDriver(){
+    drivers.shift()
+}
+
+function appendDriver(name){
+    return [...drivers, name];
+}
+
+function prependDriver(name){
+    return [name, ...drivers]
+}
+
+function removeLastDriver(){
+    return (drivers.slice(0, drivers.length - 1))
+}
+
+function removeFirstDriver(){
+    return (drivers.slice(1))
 }
